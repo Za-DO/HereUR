08fbf02e356455bf43259b8c36c276d36f88de4d
Eunbi Cho <moomb70@gmail.com>
2022-08-11 17:30:26 +0900

    [feat] Info.plist추가 및 Extensions에 컬러, 폰트 설정

---
+++ HearUR/HearUR/Extensions/ColorExtesion.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtesion.swift
+//  HearUR
+//
+//  Created by 조은비 on 2022/08/11.
+//
+
+import SwiftUI
+
+extension Color {
+    static let grayScale1 = Color("grayscale1")
+    static let grayScale2 = Color("grayscale2")
+    static let grayScale3 = Color("grayscale3")
+}
---
+++ HearUR/HearUR/Extensions/FontExtension.swift
@@ -0,0 +1,17 @@
+//
+//  FontExtension.swift
+//  HearUR
+//
+//  Created by 조은비 on 2022/08/11.
+//
+
+import SwiftUI
+
+extension Font {
+    static func customTitle() -> Font {
+        return Font.custom("D2CodingBold", size: 20)
+    }
+    static func customBody() -> Font {
+        return Font.custom("D2Coding", size: 16)
+    }
+}
